43908a1d7a65823c4a43b7a5dc4ea38ef3346b6f
Fernando González <fernando.gonzalez@geomati.co>
2014-03-15 13:02:50 +0100

    OpenLayers map plugin

--- brick-loader/src/test/java/de/csgis/geobricks/manual/HelloWorldDemo.java
+++ brick-loader/src/test/java/de/csgis/geobricks/manual/HelloWorldDemo.java
@@ -10,19 +10,19 @@ public class HelloWorldDemo {
 	public static void main(String[] args) throws Exception {
 		ServerManager serverManager = new ServerManager();
 		serverManager.start("geobricks");
-		RestPoint apps = new RestPoint(serverManager, "apps");
+		RestPoint apps = new RestPoint(serverManager, "rest/apps");
 		apps.doPut("stadtplan");
 		RestPoint plugins = new RestPoint(serverManager,
-				"apps/stadtplan/plugins");
+				"rest/apps/stadtplan/plugins");
 		Scanner sc = new Scanner(System.in);
 		System.out.println("Enter to add hello.js plugin");
-		sc.nextLine();
-		plugins.doPut("hello");
+		// sc.nextLine();
+		plugins.doPut("olmap");
 		System.out.println("plugin added");
 		System.out.println("Enter to remove the plugin");
 		sc.nextLine();
 
-		plugins.doDelete("hello");
+		plugins.doDelete("olmap");
 		System.out.println("plugin removed");
 		System.out.println("Enter to stop server");
 		sc.nextLine();
