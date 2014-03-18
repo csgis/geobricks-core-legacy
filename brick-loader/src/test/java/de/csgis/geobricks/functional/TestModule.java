584acecd1fc45e6f40c9f1a471b97418e28cd1e3
Víctor González <victor.gonzalez@geomati.co>
2014-03-18 12:20:36 +0100

    Remove useless 'Geobricks' prefix from class

--- brick-loader/src/test/java/de/csgis/geobricks/functional/TestModule.java
+++ brick-loader/src/test/java/de/csgis/geobricks/functional/TestModule.java
@@ -2,7 +2,7 @@ package de.csgis.geobricks.functional;
 
 import de.csgis.geobricks.guice.RuntimeModule;
 
-public class GeobricksTestModule extends RuntimeModule {
+public class TestModule extends RuntimeModule {
 
 	@Override
 	protected String getPersistenceUnitName() {
