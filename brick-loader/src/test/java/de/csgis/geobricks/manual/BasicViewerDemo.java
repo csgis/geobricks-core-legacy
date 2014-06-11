b8346bdff2903298d6e7f3c00b6232566a004c84
Víctor González <victor.gonzalez@geomati.co>
2014-06-11 11:19:20 +0200

    Add autocomplete ui component. It will be used by the 'Search Gemeinde' functionality. Refs #1014

--- brick-loader/src/test/java/de/csgis/geobricks/manual/BasicViewerDemo.java
+++ brick-loader/src/test/java/de/csgis/geobricks/manual/BasicViewerDemo.java
@@ -42,6 +42,7 @@ public class BasicViewerDemo {
 						"ui : ["
 								+ "{ eventName : 'ui-accordion:create', div : 'layers-accordion', parentDiv : 'layout-side'},"
 								+ "{ eventName : 'ui-html', div : 'title', parentDiv : 'layout-header', html: 'Basic viewer'},"
+								+ "{ eventName : 'ui-autocomplete:create', div : 'gemeinde_autocomplete', parentDiv : 'layout-header', sendEventName : 'search-gemeinde', options : ['gemeinde', 'another_gemeinde']},"
 								+ "{ eventName : 'ui-login:create', div : 'login', parentDiv : 'layout-header', css : 'login' }"
 								+ "]"));
 
