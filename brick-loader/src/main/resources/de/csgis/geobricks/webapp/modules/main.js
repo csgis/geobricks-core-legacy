cdf80d7c0c1fc40f04ff8660b074fa5f632f7776
Víctor González <victor.gonzalez@geomati.co>
2014-06-12 11:12:20 +0200

    Increase RequireJS timeout and show proper message on error. Fixes #1029

--- brick-loader/src/main/resources/de/csgis/geobricks/webapp/modules/main.js
+++ brick-loader/src/main/resources/de/csgis/geobricks/webapp/modules/main.js
@@ -3,8 +3,30 @@ require.config({
 	paths : {
 		$nonRequireJSDependencies
 	},
-	shim : {}
+	shim : {},
+	waitSeconds : 15
 });
+
+require.onError = function(err) {
+	var msg;
+	if (err.requireType == "timeout") {
+		msg = "Timeout downloading web components, try reloading.";
+	} else {
+		msg = "Internal error.";
+	}
+
+	var body = document.body;
+
+	var shade = document.createElement("div");
+	shade.className = "fatal_error_shade";
+
+	var text = document.createElement("div");
+	text.appendChild(document.createTextNode(msg));
+	text.className = "fatal_error";
+
+	body.appendChild(shade);
+	body.appendChild(text);
+}
 
 define([ "module", "message-bus" ], function(module, bus) {
 	var moduleList = module.config();
