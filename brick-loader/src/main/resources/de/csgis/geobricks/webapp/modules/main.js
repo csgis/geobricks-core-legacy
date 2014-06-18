8eec4e8bd9be8b8a2faa578138245bea9f63ba08
Víctor González <victor.gonzalez@geomati.co>
2014-06-18 18:30:50 +0200

    Custom management of RequireJS exceptions on main.js only for timeouts, not every exception

--- brick-loader/src/main/resources/de/csgis/geobricks/webapp/modules/main.js
+++ brick-loader/src/main/resources/de/csgis/geobricks/webapp/modules/main.js
@@ -7,27 +7,28 @@ require.config({
 	waitSeconds : 15
 });
 
-require.onError = function(err) {
-	var msg;
-	if (err.requireType == "timeout") {
-		msg = "Timeout downloading web components, try reloading.";
-	} else {
-		msg = "Internal error.";
-	}
 
+var defaultOnError = require.onError;
+
+require.onError = function(err) {
 	var body = document.body;
 
 	var shade = document.createElement("div");
 	shade.className = "fatal_error_shade";
 
 	var text = document.createElement("div");
-	text.appendChild(document.createTextNode(msg));
 	text.className = "fatal_error";
 
 	body.appendChild(shade);
 	body.appendChild(text);
 
-	console.error(err);
+	if (err.requireType == "timeout") {
+		text.appendChild(document.createTextNode("Timeout downloading web components, try reloading."));
+		console.error(err);
+	} else {
+		text.appendChild(document.createTextNode("Internal error."));
+		defaultOnError(err);
+	}
 }
 
 define([ "module", "message-bus" ], function(module, bus) {
