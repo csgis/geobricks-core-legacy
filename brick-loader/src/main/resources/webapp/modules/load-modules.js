c964775daba786f1340d62e041fec7e30c90069e
Víctor González <victor.gonzalez@geomati.co>
2014-11-13 09:23:20 +0100

    Remove console.log instructions

--- brick-loader/src/main/resources/webapp/modules/load-modules.js
+++ brick-loader/src/main/resources/webapp/modules/load-modules.js
@@ -1,8 +1,6 @@
 define([ "module", "message-bus" ], function(module, bus) {
 	var moduleList = module.config();
-	console.log(moduleList);
 	require(moduleList, function() {
-		console.log("Yay!");
 		bus.send("modules-loaded");
 	});
 });
