568244c53a887fe8ebf10fb43184477f07008716
Víctor González <victor.gonzalez@geomati.co>
2014-11-26 11:20:36 +0100

    Include .css files from src/main/webapp/styles directory for applications. Refs #1420

--- brick-loader/src/main/java/de/csgis/geobricks/servlet/client/IndexReplaceCSSFilter.java
+++ brick-loader/src/main/java/de/csgis/geobricks/servlet/client/IndexReplaceCSSFilter.java
@@ -1,11 +1,14 @@
 package de.csgis.geobricks.servlet.client;
 
+import java.io.File;
+import java.io.FilenameFilter;
 import java.io.IOException;
 
 import javax.inject.Singleton;
 import javax.servlet.Filter;
 import javax.servlet.FilterChain;
 import javax.servlet.FilterConfig;
+import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
@@ -16,12 +19,25 @@ import de.csgis.geobricks.PluginDescriptor;
 
 @Singleton
 public class IndexReplaceCSSFilter implements Filter {
+	public static final String STYLES_DIR = "styles";
 
 	private PluginDescriptor[] descriptors;
 
+	private File stylesDir;
+
 	@Override
 	public void init(FilterConfig filterConfig) throws ServletException {
-		descriptors = (PluginDescriptor[]) filterConfig.getServletContext()
+		ServletContext context = filterConfig.getServletContext();
+
+		String path = context.getRealPath("/" + STYLES_DIR);
+		if (path != null) {
+			File dir = new File(path);
+			if (dir.exists() && dir.isDirectory() && dir.canRead()) {
+				stylesDir = dir;
+			}
+		}
+
+		descriptors = (PluginDescriptor[]) context
 				.getAttribute(Geobricks.ATTR_PLUGINS_DESC);
 	}
 
@@ -31,15 +47,38 @@ public class IndexReplaceCSSFilter implements Filter {
 		CharResponseWrapper wrapper = new CharResponseWrapper(
 				(HttpServletResponse) response);
 		chain.doFilter(request, wrapper);
-		response.getWriter().print(process(wrapper.toString(), descriptors));
+		response.getWriter().print(
+				process(wrapper.toString(), descriptors, stylesDir));
 	}
 
-	public String process(String content, PluginDescriptor[] descriptors) {
+	public String process(String content, PluginDescriptor[] descriptors,
+			File stylesDir) {
 		StringBuilder str = new StringBuilder();
 		for (PluginDescriptor descriptor : descriptors) {
 			for (String style : descriptor.getStyles()) {
 				str.append("<link rel=\"stylesheet\" href=\"" + style
 						+ "\"/>\n");
+			}
+		}
+
+		if (stylesDir != null) {
+			File[] cssFiles = stylesDir.listFiles(new FilenameFilter() {
+				@Override
+				public boolean accept(File dir, String name) {
+					return name.toLowerCase().endsWith(".css");
+				}
+			});
+
+			if (cssFiles != null) {
+				for (File css : cssFiles) {
+					String path = css.getPath();
+					String stylesPath = stylesDir.getPath();
+					if (path.startsWith(stylesPath)) {
+						path = path.substring(stylesPath.length() + 1);
+					}
+					str.append("<link rel=\"stylesheet\" href=\"" + STYLES_DIR
+							+ "/" + path + "\"/>\n");
+				}
 			}
 		}
 
--- brick-loader/src/test/java/de/csgis/geobricks/servlet/client/IndexReplaceCSSFilterTest.java
+++ brick-loader/src/test/java/de/csgis/geobricks/servlet/client/IndexReplaceCSSFilterTest.java
@@ -2,7 +2,12 @@ package de.csgis.geobricks.servlet.client;
 
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
+import static org.mockito.Matchers.any;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
+import java.io.File;
+import java.io.FilenameFilter;
 import java.util.Collections;
 import java.util.regex.Pattern;
 
@@ -31,7 +36,7 @@ public class IndexReplaceCSSFilterTest {
 		String content = IOUtils
 				.toString(getClass().getResourceAsStream(INDEX));
 		String processed = filter.process(content,
-				new PluginDescriptor[] { descriptor });
+				new PluginDescriptor[] { descriptor }, mock(File.class));
 
 		assertTrue(content.contains("$styleSheets"));
 		assertFalse(processed.contains("$styleSheets"));
@@ -45,11 +50,31 @@ public class IndexReplaceCSSFilterTest {
 		String content = IOUtils
 				.toString(getClass().getResourceAsStream(INDEX));
 		String processed = filter.process(content,
-				new PluginDescriptor[] { new PluginDescriptor() });
+				new PluginDescriptor[] { new PluginDescriptor() },
+				mock(File.class));
 
 		assertTrue(content.contains("$styleSheets"));
 		assertFalse(processed.contains("$styleSheets"));
 		assertFalse(processed.contains("<link"));
+	}
+
+	@Test
+	public void testCSSFromStylesDir() throws Exception {
+		File[] files = new File[] { new File(IndexReplaceCSSFilter.STYLES_DIR
+				+ File.separator + "testing.css") };
+
+		File configDir = mock(File.class);
+		when(configDir.listFiles(any(FilenameFilter.class))).thenReturn(files);
+		when(configDir.getPath()).thenReturn(IndexReplaceCSSFilter.STYLES_DIR);
+
+		String content = IOUtils
+				.toString(getClass().getResourceAsStream(INDEX));
+		String processed = filter.process(content,
+				new PluginDescriptor[] { new PluginDescriptor() }, configDir);
+
+		assertTrue(content.contains("$styleSheets"));
+		assertFalse(processed.contains("$styleSheets"));
+		checkCSS(processed, IndexReplaceCSSFilter.STYLES_DIR + "/testing.css");
 	}
 
 	private void checkCSS(String content, String css) {
